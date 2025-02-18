4edb59f82e5127e70149ee6659d8fae5c01430c6
srinivasarao <kunchala1971@gmail.com>
2025-02-18 10:13:25 +0530

    useEffectHook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from 'react'
-import UseContextAPIHook from './Hooks/UseContextAPIHook'
+import React from "react";
+import UseEffectHook from "./Hooks/UseEffectHook";
+// import UseContextAPIHook from './Hooks/UseContextAPIHook'
 // import UseStateHook from './Hooks/UseStateHook'
 
 const App = () => {
-	return (
-		<div>
-			{/* <UseStateHook/> */}
-			<UseContextAPIHook/>
-		</div>
-	)
-}
+  return (
+    <div>
+      {/* <UseStateHook/> */}
+      {/* <UseContextAPIHook/> */}
+      <UseEffectHook />
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/Hooks/UseEffectHook/index.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import "./index.css"
+const BASEURL = "https://jsonplaceholder.typicode.com/posts";
+const UseEffectHook = () => {
+  const [data, setData] = useState([]);
+  const [input, setInput] = useState(' ');
+  useEffect(() => {
+    const fetchData = () => {
+      fetch(BASEURL)
+        .then((response) => response.json())
+        .then((data) => {
+          setData(data.filter((user) => user.title.includes(input)));
+        });
+    
+    };
+    fetchData();
+  }, [input]);
+  return (
+    <div>
+      <h1>Filter Data</h1>
+      <input type="text" onChange={(e) => setInput(e.target.value)} />
+      <ul className="column">
+        {data.map((user) => (
+          <div className="row">
+            <li>{user.id}</li>
+            <li>{user.title}</li>
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UseEffectHook;
